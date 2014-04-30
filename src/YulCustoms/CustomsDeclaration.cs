da844de71fabdc4c0c2130855ac0c54b5a6b15a5
Simon Laroche <laroche.simon@gmail.com>
2014-04-30 18:50:48 -0400

    Residency message router

---
+++ src/YulCustoms/CustomsDeclaration.cs
@@ -0,0 +1,72 @@
+﻿namespace YulCustoms
+{
+    using System;
+
+    using Newtonsoft.Json.Linq;
+
+    public class CustomsDeclaration : IMessage
+    {
+        private readonly JObject jObject;
+
+        public CustomsDeclaration():this(new JObject())
+        {
+            
+        }
+
+        public CustomsDeclaration(JObject jObject)
+        {
+            this.jObject = jObject;
+        }
+
+        public Guid Id {
+            get
+            {
+                return   this.jObject.Value<Guid>("id");
+            }
+            set
+            {
+                this.jObject["id"] = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.jObject.Value<string>("name");
+            }
+            set
+            {
+                this.jObject["name"] = value;
+            }
+        }
+
+        public string Citenzenship
+        {
+            get
+            {
+                return this.jObject.Value<string>("citenzenship");
+            }
+            set
+            {
+                this.jObject["citenzenship"] = value;
+            }
+        }
+
+        public bool Resident
+        {
+            get
+            {
+                return this.jObject.Value<bool>("resident");
+            }
+            set
+            {
+                this.jObject["resident"] = value;
+            }
+        }
+
+
+
+
+    }
+}
---
+++ src/YulCustoms/ResidencyRouter.cs
@@ -0,0 +1,27 @@
+﻿namespace YulCustoms
+{
+    public class ResidencyRouter: IHandle<CustomsDeclaration>{
+        private readonly IHandle<CustomsDeclaration> residentHandler;
+
+        private readonly IHandle<CustomsDeclaration> visitorHandler;
+
+        public ResidencyRouter(IHandle<CustomsDeclaration> residentHandler, IHandle<CustomsDeclaration> visitorHandler)
+        {
+            this.residentHandler = residentHandler;
+            this.visitorHandler = visitorHandler;
+        }
+
+        public void Handle(CustomsDeclaration message)
+        {
+            if (message.Resident)
+            {
+                this.residentHandler.Handle(message);
+            }
+            else
+            {
+                this.visitorHandler.Handle(message);
+            }
+
+        }
+    }
+}
